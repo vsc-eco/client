0e70b9db4f3d69bbfeb02014ed6f1a60c70002ce
vaultec <47548474+vaultec81@users.noreply.github.com>
2024-02-03 19:36:43 -0800

    tweak (convertTxJws): support multiple signatures to JWS

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ export async function convertTxJws(args: {
     tx: string
   }) {
     const tx = Buffer.from(args.tx, 'base64url')
-    const sig = DagCbor.util.deserialize(Buffer.from(args.sig, 'base64url')) as {
+    const sigDecoded = DagCbor.util.deserialize(Buffer.from(args.sig, 'base64url')) as {
         __t: 'vsc-sig',
         sigs: [
             {
@@ -18,21 +18,24 @@ export async function convertTxJws(args: {
   
     const cid = (await DagCbor.util.cid(tx))
 
-    let jwsDag = {
-        jws: {
-          payload: Buffer.from(cid.bytes).toString('base64url'),
-          signatures: [
-                {
-                    protected: Buffer.from(JSON.stringify({
-                        alg: sig.sigs[0].alg,
-                        kid: [sig.sigs[0].kid, sig.sigs[0].kid.split(':')[2]].join('#')
-                    })).toString('base64url'),
-                    signature: sig.sigs[0].sig
-                }
-          ],
-            link: cid
-        },
-        linkedBlock: tx
+    let jwsDagOutput = []
+    for(const sigVal of sigDecoded.sigs) {
+        jwsDagOutput.push({
+            jws: {
+              payload: Buffer.from(cid.bytes).toString('base64url'),
+              signatures: [
+                    {
+                        protected: Buffer.from(JSON.stringify({
+                            alg: sigVal.alg,
+                            kid: [sigVal.kid, sigVal.kid.split(':')[2]].join('#')
+                        })).toString('base64url'),
+                        signature: sigVal.sig
+                    }
+              ],
+                link: cid
+            },
+            linkedBlock: tx
+        })
     }
-    return jwsDag
+    return jwsDagOutput
   }
